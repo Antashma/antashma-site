2724d1694fd3a90d5f13da2c3662fdef1dd0a33a
Samantha G <antashma@gmail.com>
2025-07-30 07:48:42 -0400

    updated projects section

--- src/pages/ProjectsContainer.jsx
+++ src/pages/ProjectsContainer.jsx
@@ -3,11 +3,18 @@ export default function ProjectsContainer() {
         <section id="projects">
             <h2>Projects</h2>
             <div>
+                <h3>PokEvalute</h3>
+                <p>Select and get details on your favorite Pokemon team. Then get an evaluation from AI!</p>
+                <a href="https://pokevaluate-fe.vercel.app/" target="_blank">https://pokevaluate-fe.vercel.app/</a>
+            </div>
+            <div>
                 <h3>Get The Sequence</h3>
+                <p>Enter the correct sequence of numbers within the time limit!</p>
                 <a href="https://get-the-sequence.vercel.app/" target="_blank">https://get-the-sequence.vercel.app/</a>
             </div>
             <div>
                 <h3>To Do List</h3>
+                <p>Keep track of your daily tasks!</p>
                 <a href="https://react-todo-antashma.vercel.app/" target="_blank">https://react-todo-antashma.vercel.app/</a>
             </div>
         </section>
