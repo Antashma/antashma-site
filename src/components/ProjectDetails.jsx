e8068ce3a2f883154326b927ecc29963ae48a70f
Samantha G <antashma@gmail.com>
2025-10-28 22:20:32 -0400

    update projects style

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,10 @@
+export default function ProjectDetails({ projectData }) {
+    return (
+        <div className="project-details">
+            <h3>{projectData.name}</h3>
+            <p>{projectData.description}</p>
+            <p className="tech-stack">{projectData.techStack.join(", ")}</p>
+            <a className="cta-link" href={projectData.url} target="_blank">{projectData.url}</a>
+        </div>
+    )
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,39 +1,21 @@
-import { useState } from "react";
 import projects from "../data/projects";
 import WindowTitleBar from "../components/WindowTitleBar";
+import ProjectDetails from "../components/ProjectDetails";
 
 
 export default function Projects() {
-    const [currentProject, setCurrentProject] = useState(projects[0]);
 
     const style = {
         display: "none"
-    }
-
-    function handleClick(id){
-        const found = projects.find(p => p.id === id);
-        setCurrentProject(found);
     }
 
     return (
         <section id="projects" className="window" style={style}>
             <WindowTitleBar titleText="📂Projects/" windowId="projects" />
             <div className="content">
-
-                <nav id="project-nav">
-                    <ul>
-                        <li onClick={() => handleClick(0)}>PokEvalute</li>
-                        <li onClick={() => handleClick(1)}>Get The Sequence</li>
-                        <li onClick={() => handleClick(2)}>To Do List</li>
-                    </ul>
-                </nav>
-
-                <div className="project-details">
-                    <h3>{currentProject.name}</h3>
-                    <p>{currentProject.description}</p>
-                    <p className="tech-stack">{currentProject.techStack.join(", ")}</p>
-                    <a className="cta-link" href={currentProject.url} target="_blank">{currentProject.url}</a>
-                </div>
+                {
+                    projects.map(project => (<ProjectDetails key={project.id} projectData={project} />))
+                } 
             </div> 
         </section>
     )
